b7f5023fcd29ef6e77aa91004e213102532a2923
mlitman <awesomefat@gmail.com>
2018-11-15 11:23:00 -0600

    rot13

--- app/src/main/java/com/example/aweso/csc250androidstarter/MainActivity.java
+++ app/src/main/java/com/example/aweso/csc250androidstarter/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -18,32 +19,102 @@ public class MainActivity extends AppCompatActivity
         this.setContentView(R.layout.activity_main);
         //this - how an object refers to itself from within itself
         //this is this particular instance of Activity
-
-
     }
 
-    public void someButtonWasPressed(View v)
+    public void goButtonPressed(View v)
     {
-        Button b = (Button)v;
-        TextView appNameTV = (TextView)this.findViewById(R.id.appNameTV);
-        appNameTV.setText(b.getText().toString());
+        EditText inputET = (EditText)this.findViewById(R.id.inputET);
+        TextView outputTV = (TextView)this.findViewById(R.id.outputTV);
+        String input = inputET.getText().toString();
+        outputTV.setText("" + this.rot13_4(input.charAt(0)));
+        //outputTV.setText("" + (int)input.charAt(0));
     }
 
-    public void lolButtonPressed(View v)
+    //Encryption Algorithm
+    //Ceasar Cipher - rot13 stands for Rotate 13
+    public char rot13(char c)
     {
-        TextView appNameTV = (TextView)this.findViewById(R.id.appNameTV);
-        appNameTV.setText("LOL");
+        c = Character.toUpperCase(c);
+        String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String map =   "NOPQRSTUVWXYZABCDEFGHIJKLM";
+
+        //find position of c in alpha
+        int pos = -1;
+        for(int i = 0; i < alpha.length(); i++)
+        {
+            if(alpha.charAt(i) == c)
+            {
+                pos = i;
+                break;
+            }
+        }
+        //now pos should hold the position of c in alpha
+        return map.charAt(pos);
     }
 
-    public void wootButtonPressed(View v)
+    public char rot13_2(char c)
     {
-        TextView appNameTV = (TextView)this.findViewById(R.id.appNameTV);
-        appNameTV.setText("WOOT");
+        c = Character.toUpperCase(c);
+        String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        //find position of c in alpha
+        int pos = -1;
+        for(int i = 0; i < alpha.length(); i++)
+        {
+            if(alpha.charAt(i) == c)
+            {
+                pos = i;
+                break;
+            }
+        }
+        //now pos should hold the position of c in alpha
+        return alpha.charAt((pos+13)%26);
     }
 
-    public void blaineButtonPressed(View v)
+    public char rot13_3(char c)
     {
-        TextView appNameTV = (TextView)this.findViewById(R.id.appNameTV);
-        appNameTV.setText("BLAINE");
+        String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        //now pos should hold the position of c in alpha
+        return alpha.charAt((alpha.indexOf(c)+13)%26);
     }
+
+    public char rot13_4(char c)
+    {
+        return (char)(65 + ((((int)c) + 13) % 26));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
